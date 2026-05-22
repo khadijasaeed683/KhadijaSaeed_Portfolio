4427eb2ade6b157d9ead19088f480993839e013a
unknown <khadijasaeed683@gmail.com>
2026-05-22 23:44:55 +0500

    Fix vercel CI error

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,8 +8,8 @@ import {
   workExperiences,
   skillsSection,
   openSource,
-  blogSection,
-  talkSection,
+  // blogSection,
+  // talkSection,
   achievementSection,
   resumeSection
 } from "../../portfolio";
@@ -20,8 +20,8 @@ function Header() {
   const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
-  const viewBlog = blogSection.display;
-  const viewTalks = talkSection.display;
+  // const viewBlog = blogSection.display;
+  // const viewTalks = talkSection.display;
   const viewResume = resumeSection.display;
 
   return (
